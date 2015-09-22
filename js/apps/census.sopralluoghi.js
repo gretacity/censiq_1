2d5729bd90c3547862ebf5868525ad50741e25b3
Utente <Utente@Utente-PC>
2015-09-22 08:34:51 +0200

    Primo commit

--- js/apps/census.sopralluoghi.js
+++ js/apps/census.sopralluoghi.js
@@ -974,7 +974,7 @@ var app = {
                     var id="";
                     try
                     {
-                        id=eval(r.id)
+                        id=eval('r.id')
                     }
                     catch(e){}
                     html += '<li><a href="' + ref + '" id="sp'+id+'">' + text + '</a></li>';
